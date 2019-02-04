86c4c7117a975e650e9c6f93958f7df2a8a1e038
Michael Naraiswar <naraiswar@gmail.com>
2019-02-04 02:55:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line (array)
   if array.count == 0
     puts "The line is currently empty."
   else
-    msg = "The line is currently: "
-    array.each {|name| msg << "#{array.index(name)+1}. #{name} "}
+    msg = "The line is currently:"
+    array.each {|name| msg << " #{array.index(name)+1}. #{name}"}
     print msg
   end
 end
