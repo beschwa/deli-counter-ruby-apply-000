963f5af9ab140b255479cb65d998cbbd45e39043
Michael Naraiswar <naraiswar@gmail.com>
2019-02-04 03:28:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,18 @@ def line (array)
     puts msg
   end
 end
+
+def take_a_number (array, name)
+  array << name
+  msg = "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  puts msg
+end
+
+def now_serving (array)
+  if array.count == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.delete_at(0)
+  end
+end
